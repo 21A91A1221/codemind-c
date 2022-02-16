e8a2640b6abdcad9664ca89fe1acf2718547e7f7
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-02-16 19:37:28 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')     {         printf("Vowel",ch);     }     else     {         printf("Consonant",ch);     }     return 0; }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("Vowel",ch);
+    }
+    else
+    {
+        printf("Consonant",ch);
+    }
+    return 0;
+}
