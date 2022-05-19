21d34816ad812b743095ede4f666b205eb8a7929
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-05-19 10:56:53 +0530

    #include<stdio.h> int main() {     int a[100],i,t,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--)     {         if(a[i]%2!=0)         {            t=a[i];            break;         }     }     printf("%d",t);  }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,t,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2!=0)
+        {
+           t=a[i];
+           break;
+        }
+    }
+    printf("%d",t);
+ }
