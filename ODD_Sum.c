cf2edbe06f708cdcb9fe19c6f54ca6fc66839774
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-05-20 11:39:46 +0530

    #include<stdio.h> int main() {     int a[100],i,c,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             c+=a[i];         }     }     printf("%d",c); }

---
+++ ODD_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,c,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            c+=a[i];
+        }
+    }
+    printf("%d",c);
+}
