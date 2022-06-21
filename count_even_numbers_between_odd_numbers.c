8754ac83649e8d59ddf2bc89ec34627e006e47c6
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2022-06-21 11:26:16 +0530

    #include<stdio.h> int main() {     int n,k,v[500],r=0;     scanf("%d",&n);     for(k=0;k<n;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<n-2;k++)     {         if(v[k]%2!=0 && v[k+1]%2==0 && v[k+2]%2!=0)         {             r++;         }     }     printf("%d",r); }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,v[500],r=0;
+    scanf("%d",&n);
+    for(k=0;k<n;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=0;k<n-2;k++)
+    {
+        if(v[k]%2!=0 && v[k+1]%2==0 && v[k+2]%2!=0)
+        {
+            r++;
+        }
+    }
+    printf("%d",r);
+}
