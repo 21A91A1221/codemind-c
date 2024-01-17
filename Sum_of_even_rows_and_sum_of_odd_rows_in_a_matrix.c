ad512a8f66a50134a1b9a7a57e9b0e2232bb8814
NitinReddy <97592122+21A91A1221@users.noreply.github.com>
2024-01-17 11:53:21 +0530

    #include<stdio.h> int main() {     int i,j,r,c,e=0,o=0;     scanf("%d %d",&r,&c);     int arr[r][c];     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i%2==0)             {                 e=e+arr[i][j];             }             else             {                 o=o+arr[i][j];             }         }     }     printf("%d %d",e,o);     return 0; }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,r,c,e=0,o=0;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i%2==0)
+            {
+                e=e+arr[i][j];
+            }
+            else
+            {
+                o=o+arr[i][j];
+            }
+        }
+    }
+    printf("%d %d",e,o);
+    return 0;
+}
